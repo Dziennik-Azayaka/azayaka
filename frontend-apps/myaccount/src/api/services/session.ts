8763cb2dbde905d965219109b2b54905fe724191
Marioneq4958 <mq4958@gmail.com>
2025-07-23 21:26:01 +0200

    Rebase

--- frontend-apps/myaccount/src/api/services/session.ts
+++ frontend-apps/myaccount/src/api/services/session.ts
@@ -1,7 +1,7 @@
 import axios from "@/api";
-import type { SessionListDTO } from "@/api/dto/session-list.ts";
 import { ApiError } from "@/api/errors.ts";
 import { sessionListDTOToEntity } from "@/api/mappers/session-list.ts";
+import type { SessionListDTO } from "@/api/dto/session-list.ts";
 
 export default {
     getActiveSession: () =>
