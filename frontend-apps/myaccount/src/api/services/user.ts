1b3e80186ab7f5914c65b9fea955bf8c76773542
Marioneq4958 <mq4958@gmail.com>
2025-07-27 11:09:04 +0200

    Add API using and missing translations in changing email address

--- frontend-apps/myaccount/src/api/services/user.ts
+++ frontend-apps/myaccount/src/api/services/user.ts
@@ -1,10 +1,11 @@
-import { ApiError } from "../errors";
+import { AlreadyTakenEmailAddressError, ApiError, IncorrectPasswordError } from "../errors";
 import { activityLogEntryDTOToEntity } from "../mappers/activity-log-entry";
 
 import axios from "@/api";
 import type { ActivityLogEntryDTO } from "@/api/dto/activity-log-entry.ts";
 import type { PaginatedResourceDTO } from "@/api/dto/paginated-resource.ts";
 import { paginationResourceDTOToEntity } from "@/api/mappers/paginated-resource.ts";
+import { AxiosError } from "axios";
 
 export default {
     getActivityLog: (page: number) =>
@@ -13,7 +14,21 @@ export default {
             .then(({ data: response }) =>
                 paginationResourceDTOToEntity(response, response.data.map(activityLogEntryDTOToEntity)),
             )
-            .catch(({ reason }) => {
+            .catch((reason) => {
                 throw new ApiError(reason);
             }),
+	setNewEmailAddress: (newEmailAddress: string, password: string) =>
+		axios
+			.put("/user/email", { email: newEmailAddress, password })
+			.catch((reason) => {
+				if (reason instanceof AxiosError) {
+					console.log(reason.response?.data.errors)
+					const errors = reason.response?.data.errors;
+					if ('email' in errors && errors.email.includes("The email has already been taken.")) throw new AlreadyTakenEmailAddressError();
+					if (Array.isArray(errors)) {
+						if (errors.includes("WRONG_PASSWORD")) throw new IncorrectPasswordError();
+					}
+				}
+				throw new ApiError(reason);
+			})
 };
--- frontend-apps/myaccount/src/components/ChangeEmailDialog.vue
+++ frontend-apps/myaccount/src/components/ChangeEmailDialog.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import PasswordInput from "#ui/components/ui/input/PasswordInput.vue";
 import { toTypedSchema } from "@vee-validate/zod";
 import { useForm } from "vee-validate";
 import { ref } from "vue";
@@ -22,8 +23,14 @@ import {
     FormMessage,
     Input,
 } from "@azayaka-frontend/ui";
+import UserApiService from "@/api/services/user";
+import { AlreadyTakenEmailAddressError, IncorrectPasswordError } from "@/api/errors";
+import { useMainStore } from "@/stores/main.store";
+import ErrorBanner from "./ErrorBanner.vue";
 
 const { t } = useI18n();
+const mainStore = useMainStore();
+const showDialog = ref(false);
 
 const formSchema = toTypedSchema(
     z.object({
@@ -32,31 +39,48 @@ const formSchema = toTypedSchema(
                 required_error: t("requiredEmailAddressError"),
             })
             .email(t("invalidEmailAddressError")),
+		password: z
+			.string({
+                required_error: t("requiredPasswordError"),
+            })
     }),
 );
 const form = useForm({
     validationSchema: formSchema,
 });
 
-const onSubmit = form.handleSubmit((values) => {
-    console.log(values);
-});
 const isLoading = ref(false);
+const error = ref<string | null>();
+
+const onSubmit = form.handleSubmit(async (values) => {
+    isLoading.value = true;
+	error.value = null;
+	try {
+		await UserApiService.setNewEmailAddress(values.emailAddress, values.password);
+		mainStore.emailAddress = values.emailAddress;
+		showDialog.value = false;
+
+	} catch (reason) {
+		if (reason instanceof IncorrectPasswordError) error.value = "incorrectPasswordError";
+		else if (reason instanceof AlreadyTakenEmailAddressError) error.value = "alreadyTakenEmailAddressError"
+		else error.value = "unknownError";
+	} finally {
+		isLoading.value = false;
+	}
+});
+
 </script>
 
 <template>
-    <Dialog>
+    <Dialog v-model:open="showDialog">
         <DialogTrigger as-child>
             <Button size="sm" variant="secondary">{{ t("change") }}</Button>
         </DialogTrigger>
         <DialogContent>
             <form @submit="onSubmit" class="space-y-3">
                 <DialogHeader>
-                    <DialogTitle>Zmień adres e-mail</DialogTitle>
-                    <DialogDescription
-                        >Ustaw nowy adres e-mail, który będziesz używać do logowania się do dziennika
-                        Azayaka.</DialogDescription
-                    >
+                    <DialogTitle>{{ t("changeEmailAddress") }}</DialogTitle>
+                    <DialogDescription>{{ t("changeEmailAddressDescription") }}</DialogDescription>
                 </DialogHeader>
                 <FormField v-slot="{ componentField }" name="emailAddress">
                     <FormItem>
@@ -67,11 +91,21 @@ const isLoading = ref(false);
                         <FormMessage />
                     </FormItem>
                 </FormField>
+                <FormField v-slot="{ componentField }" name="password">
+                    <FormItem>
+                        <FormLabel>{{ t("currentPassword") }}</FormLabel>
+                        <FormControl>
+                            <PasswordInput v-bind="componentField" :disabled="isLoading" autocapitalize="off" />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
+				<ErrorBanner v-if="error" :description="error" />
                 <DialogFooter>
                     <DialogClose as-child>
-                        <Button variant="outline" type="button">Anuluj</Button>
+                        <Button variant="outline" type="button">{{ t("cancel") }}</Button>
                     </DialogClose>
-                    <Button type="submit">Potwierdź</Button>
+                    <Button type="submit">{{ t("confirm") }}</Button>
                 </DialogFooter>
             </form>
         </DialogContent>
