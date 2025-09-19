39c456543306578ce93924f9f801392b2c258ccd
Marioneq4958 <mq4958@gmail.com>
2025-09-19 19:13:36 +0200

    Create employees view in administrator panel

---
+++ frontend-apps/administrator/src/pages/EmployeeList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import PanelHeader from "@/components/PanelHeader.vue";
+
+const { t } = useI18n();
+</script>
+
+<template>
+    <div>
+        <PanelHeader :breadcrumb-path="[{ href: { name: 'employees' }, title: t('employees') }]" />
+        <h1 class="text-2xl font-semibold">{{ t("employees") }}</h1>
+        <p class="text-foreground/70 mb-4 text-sm">{{ t("employeesDescription") }}</p>
+    </div>
+</template>
