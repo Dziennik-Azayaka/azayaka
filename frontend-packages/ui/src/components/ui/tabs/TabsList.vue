2dcaf6fa7c360b532f2f3faee1ffbc703483e1f7
Marioneq4958 <mq4958@gmail.com>
2025-09-20 12:57:00 +0200

    Format code

--- frontend-packages/ui/src/components/ui/tabs/TabsList.vue
+++ frontend-packages/ui/src/components/ui/tabs/TabsList.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
-import type { TabsListProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import { reactiveOmit } from "@vueuse/core"
-import { TabsList } from "reka-ui"
-import { cn } from '#ui/lib/utils'
+import { reactiveOmit } from "@vueuse/core";
+import type { TabsListProps } from "reka-ui";
+import { TabsList } from "reka-ui";
+import type { HTMLAttributes } from "vue";
 
-const props = defineProps<TabsListProps & { class?: HTMLAttributes["class"] }>()
+import { cn } from "#ui/lib/utils";
 
-const delegatedProps = reactiveOmit(props, "class")
+const props = defineProps<TabsListProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = reactiveOmit(props, "class");
 </script>
 
 <template>
-  <TabsList
-    data-slot="tabs-list"
-    v-bind="delegatedProps"
-    :class="cn(
-      'bg-accent text-muted-foreground inline-flex w-fit items-center justify-center rounded-lg p-1.5',
-      props.class,
-    )"
-  >
-    <slot />
-  </TabsList>
+    <TabsList
+        data-slot="tabs-list"
+        v-bind="delegatedProps"
+        :class="
+            cn(
+                'bg-accent text-muted-foreground inline-flex w-fit items-center justify-center rounded-lg p-1.5',
+                props.class,
+            )
+        "
+    >
+        <slot />
+    </TabsList>
 </template>
