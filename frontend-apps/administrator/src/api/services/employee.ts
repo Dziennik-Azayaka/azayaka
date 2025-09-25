236613b484b7063519c605008e257d6db5edbc06
Marioneq4958 <mq4958@gmail.com>
2025-09-25 17:42:20 +0200

    Add adding new employees

--- frontend-apps/administrator/src/api/services/employee.ts
+++ frontend-apps/administrator/src/api/services/employee.ts
@@ -1,7 +1,18 @@
 import axios from "@/api";
 import type { EmployeeDTO } from "@/api/dto/employee.ts";
 import { employeeDTOToEntity } from "@/api/mappers/employee";
+import type { EmployeeForm } from "@/types.ts";
 
 export default {
     getAllEmployees: () => axios.get<EmployeeDTO[]>("/employees").then((res) => res.data.map(employeeDTOToEntity)),
+    addEmployee: async (employee: EmployeeForm) =>
+        axios.post("/employees", {
+            firstName: employee.firstName,
+            lastName: employee.lastName,
+            shortcut: employee.shortcut,
+            isAdmin: employee.roles.includes("administrator"),
+            isHeadmaster: employee.roles.includes("headmaster"),
+            isSecretary: employee.roles.includes("secretary"),
+            isTeacher: employee.roles.includes("teacher"),
+        }),
 };
