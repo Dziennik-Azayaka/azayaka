adbbd8c19af3a72a92fddab2fc6c00a0d67c6a31
Marioneq 4958 <mq4958@gmail.com>
2025-12-05 18:23:10 +0100

    Create the system access view

---
+++ frontend-apps/administrator/src/pages/SystemAccess.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+import PanelHeader from "@/components/PanelHeader.vue";
+
+const { t } = useI18n();
+</script>
+
+<template>
+    <div>
+        <PanelHeader :breadcrumb-path="[{ href: { name: 'systemAccess' }, title: t('systemAccess') }]" />
+        <h1 class="text-2xl font-semibold">{{ t("systemAccess") }}</h1>
+        <p class="text-foreground/70 mb-4 text-sm">{{ t("systemAccessDescription") }}</p>
+    </div>
+</template>
