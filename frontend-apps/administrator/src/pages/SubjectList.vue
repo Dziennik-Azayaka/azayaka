aeff00f23c977563e55bc07103083610641a8c7c
Marioneq 4958 <mq4958@gmail.com>
2025-12-26 17:21:50 +0100

    Create subjects table

--- frontend-apps/administrator/src/pages/SubjectList.vue
+++ frontend-apps/administrator/src/pages/SubjectList.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
-import PanelHeader from "@/components/PanelHeader.vue";
 import { useI18n } from "vue-i18n";
 
+import PanelHeader from "@/components/PanelHeader.vue";
+import SubjectsTable from "@/components/subjects/SubjectsTable.vue";
+
 const { t } = useI18n();
+
+const subjects = [
+    {
+        id: 1,
+        name: "Matematyka",
+        shortcut: "mat.",
+        active: true
+    },
+    {
+        id: 2,
+        name: "Język angielski",
+        shortcut: "j. ang.",
+        active: false
+    },
+    {
+        id: 3,
+        name: "Tworzenie zaawansowanych aplikacji webowych",
+        shortcut: "tw. zaaw. apl. web.",
+        active: true
+    },
+];
 </script>
 
 <template>
@@ -10,5 +33,6 @@ const { t } = useI18n();
         <PanelHeader :breadcrumb-path="[{ href: { name: 'subjects' }, title: t('subjects') }]" />
         <h1 class="text-2xl font-semibold">{{ t("subjects") }}</h1>
         <p class="text-foreground/70 mb-4 text-sm">{{ t("subjectsDescription") }}</p>
+        <SubjectsTable :subjects="subjects" />
     </div>
 </template>
