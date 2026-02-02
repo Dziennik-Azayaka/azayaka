33aad7094ba8425317e8b0659215314bb07b3df7
Piotr Badełek <piotrbadelek@protonmail.com>
2026-02-02 22:00:45 +0100

    KE and KED: Write basic database schema

---
+++ app/Models/ResidenceAddress.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResidenceAddress extends Model
+{
+    /** @use HasFactory<\Database\Factories\ResidenceAddressFactory> */
+    use HasFactory;
+}
