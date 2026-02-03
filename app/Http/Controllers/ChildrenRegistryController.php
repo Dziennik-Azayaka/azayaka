33b09c287d4028c741577075926ffd9233638596
Piotr Badełek <piotrbadelek@protonmail.com>
2026-02-03 22:00:26 +0100

    KU/KED: Implement creating registries and students

--- app/Http/Controllers/ChildrenRegistryController.php
+++ app/Http/Controllers/ChildrenRegistryController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ChildrenRegistry;
 use Illuminate\Http\Request;
 
 class ChildrenRegistryController extends Controller
 {
-    //
+	public function list()
+	{
+		return ChildrenRegistry::all()->toResourceCollection();
+	}
+
+	public function create(Request $request, Int $schoolUnitId)
+	{
+		if (ChildrenRegistry::where("school_unit_id", $schoolUnitId)->exists()) {
+			return \Response::json([
+				"success" => false,
+				"errors" => [
+					"CHILDREN_REGISTRY_ALREADY_EXISTS"
+				]
+			], 409);
+		}
+
+		$registry = new ChildrenRegistry();
+		$registry->school_unit_id = $schoolUnitId;
+		$registry->save();
+
+		return \Response::json([
+			"success" => true
+		], 201);
+	}
 }
---
+++ app/Http/Resources/StudentRegistryResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentRegistryResource extends JsonResource
+{
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function toArray(Request $request): array
+	{
+		return [
+			"id" => $this->id,
+			"schoolUnitId" => $this->school_unit_id,
+			"openedOn" => $this->created_at
+		];
+	}
+}
