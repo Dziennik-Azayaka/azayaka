b19e305ceb5cedac9553230468d96b73be9213c8
Marioneq 4958 <mq4958@gmail.com>
2026-02-08 17:32:08 +0100

    Add missing translation

--- frontend-apps/administrator/src/pages/ClassificationPeriods.vue
+++ frontend-apps/administrator/src/pages/ClassificationPeriods.vue
@@ -56,7 +56,7 @@ const { data, isError, refetch, fetchStatus } = useQuery({
 
         <section class="px-3 sm:px-5 py-3 gap-2 border rounded-md flex justify-between items-center">
             <p class="font-semibold">
-                <span class="not-md:hidden">Rok szkolny</span> {{ getSchoolYearString(schoolYear) }}
+                <span class="not-md:hidden">{{ t("schoolYear") }}</span> {{ getSchoolYearString(schoolYear) }}
             </p>
             <div class="flex gap-3">
                 <ClassificationPeriodsChangeDialog
