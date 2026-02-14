a8a55b05fdbd9ad00ac5def3fcf94995c16989af
Piotr Badełek <piotrbadelek@protonmail.com>
2026-02-14 17:01:46 +0100

    Add mass imports of students, write tests for Student, StudentRegistry and ChildrenRegistry

--- app/Http/Controllers/ClassificationPeriodController.php
+++ app/Http/Controllers/ClassificationPeriodController.php
@@ -40,7 +40,7 @@ class ClassificationPeriodController extends Controller
 
 		foreach ($validated["periodEnd"] as $key => $periodEnd) {
 			if ($key == 0) {
-				$periodStart = "{$schoolYear}-09-01";
+				$periodStart = "$schoolYear-09-01";
 			} else {
 				$periodStart = Carbon::parse($validated["periodEnd"][$key - 1])->addDay()->toDateString();
 			}
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,8 +9,11 @@ use App\Models\Student;
 use App\Models\StudentRegistry;
 use App\Rules\Pesel;
 use App\Utilities\ValidatorAssistant;
+use Carbon\Carbon;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\File;
 
 class StudentController extends Controller
 {
@@ -21,24 +24,7 @@ class StudentController extends Controller
 
 	public function create(Request $request, StudentRegistry $studentRegistry)
 	{
-		$validator = ValidatorAssistant::validate($request, [
-			"firstName" => ["required", "max:255"],
-			"lastName" => ["required", "max:255"],
-			"secondName" => ["nullable", "max:255"],
-			"pesel" => ["required_without:alternateIdentityDocument", "unique:students,pesel", new Pesel],
-			"alternateIdentityDocument" => ["required_without:pesel", "max:255", "unique:students,alternate_identity_document"],
-			"birthdate" => ["required", "date"],
-			"birthplace" => ["required", "max:255"],
-			"gender" => ["required", "in:male,female"],
-			"admissionDate" => ["required", "date"],
-			"residenceAddressCountry" => ["required", "max:255"],
-			"residenceAddressCommune" => ["nullable", "max:255"],
-			"residenceAddressTown" => ["nullable", "max:255"],
-			"residenceAddressPostalCode" => ["nullable", "max:255"],
-			"residenceAddressHouseNumber" => ["nullable", "max:255"],
-			"residenceAddressStreet" => ["nullable", "max:255"],
-			"childrenRegistryId" => ["nullable", "exists:children_registries,id"]
-		]);
+		$validator = ValidatorAssistant::validate($request, $this->generateValidationRules(true, true));
 
 		if (!$validator["success"]) {
 			return $validator["errorResponse"];
@@ -46,33 +32,7 @@ class StudentController extends Controller
 
 		$validated = $validator["data"];
 
-		$residenceAddress = new ResidenceAddress();
-		$residenceAddress->country = $validated["residenceAddressCountry"];
-		$residenceAddress->commune = $validated["residenceAddressCommune"];
-		$residenceAddress->town = $validated["residenceAddressTown"];
-		$residenceAddress->postal_code = $validated["residenceAddressPostalCode"];
-		$residenceAddress->house_number = $validated["residenceAddressHouseNumber"];
-		$residenceAddress->street = $validated["residenceAddressStreet"];
-		$residenceAddress->save();
-
-		$student = new Student();
-		$student->first_name = $validated["firstName"];
-		$student->last_name = $validated["lastName"];
-		$student->second_name = $validated["secondName"];
-		$student->pesel = $validated["pesel"] ?? null;
-		$student->alternate_identity_document = $validated["alternateIdentityDocument"] ?? null;
-		$student->birthdate = $validated["birthdate"];
-		$student->birthplace = $validated["birthplace"];
-		$student->gender = $validated["gender"];
-		$student->admission_date = $validated["admissionDate"];
-		$student->residence_address_id = $residenceAddress->id;
-
-		try {
-			$student->saveOrFail();
-		} catch (\Throwable) {
-			// Saving the student failed, so we need to delete the residence address
-			$residenceAddress->delete();
-		}
+		$student = $this->createAndSaveStudentWithResidenceAddress($validated);
 
 		$studentRegistry->students()->attach($student);
 
@@ -85,30 +45,167 @@ class StudentController extends Controller
 		], 201);
 	}
 
-	public function update(Request $request, Student $student)
+	public function update(Request $request, StudentRegistry $studentRegistry, Student $student)
 	{
-		$validator = ValidatorAssistant::validate($request, [
-			"firstName" => "required|max:255",
-			"lastName" => "required|max:255",
-			"secondName" => "nullable|max:255",
-			Rule::anyOf([
-				"pesel" => "size:11|unique:students,pesel",
-				"alternateIdentityDocument" => "max:255|unique:students,alternate_identity_document",
-			]),
-			"birthdate" => "required|date",
-			"birthplace" => "required|max:255",
-			"gender" => "required|in:male,female",
-			"admissionDate" => "required|date"
-		]);
+		$validator = ValidatorAssistant::validate($request, $this->generateValidationRules(
+			false, false, $student
+		));
 
 		if (!$validator["success"]) {
 			return $validator["errorResponse"];
 		}
 
 		$validated = $validator["data"];
-		$student->update($validated);
+		$student["first_name"] = $validated["firstName"];
+		$student["last_name"] = $validated["lastName"];
+		$student["second_name"] = $validated["secondName"] ?? null;
+		$student["pesel"] = $validated["pesel"] ?? null;
+		$student["alternate_identity_document"] = $validated["alternateIdentityDocument"] ?? null;
+		$student["birthdate"] = $validated["birthdate"];
+		$student["birthplace"] = $validated["birthplace"];
+		$student["gender"] = $validated["gender"];
+		$student["admission_date"] = $validated["admissionDate"];
+		$student->save();
 		return [
 			"success" => true
 		];
+	}
+
+	public function massCreateFromCSV(Request $request, StudentRegistry $studentRegistry)
+	{
+		$validator = ValidatorAssistant::validate($request, [
+			"csv" => ["required", "file", File::types(["csv"])->max(2048)],
+			"childrenRegistryId" => ["nullable", "exists:children_registries,id"],
+			"delimiter" => ["nullable", "string"]
+		]);
+
+		if (!$validator["success"]) {
+			return $validator["errorResponse"];
+		}
+
+		$childrenRegistryId = $validator["data"]["childrenRegistryId"];
+
+		$lines = explode(PHP_EOL, trim($request->file("csv")->get()));
+		$headers = str_getcsv(array_shift($lines));
+
+		$uploadedData = array_map(function($line) use ($headers) {
+			return array_combine($headers, str_getcsv($line));
+		}, $lines);
+
+		$validationRules = $this->generateValidationRules(true);
+
+		DB::beginTransaction();
+		$transactionSuccessful = true;
+
+		$students = [];
+		foreach ($uploadedData as $row) {
+			$validator = ValidatorAssistant::validate($row, $validationRules);
+
+			// TODO: Include information about the row which contains the error
+			if (!$validator["success"]) {
+				return $validator["errorResponse"];
+			}
+			$data = $validator["data"];
+
+			// This isn't efficient; however, we need to know the IDs of addresses and students
+			// to build relationships with the registries.
+			$students[] = $this->createAndSaveStudentWithResidenceAddress($data, $transactionSuccessful);
+		}
+
+		$studentRegistry->students()->attach($students);
+		if (array_key_exists("childrenRegistryId", $validator["data"])) {
+			ChildrenRegistry::find($childrenRegistryId)->students()->attach($students);
+		}
+
+		if (!$transactionSuccessful) {
+			DB::rollBack();
+			return \Response::json([
+				"success" => true,
+				"errors" => ["UNKNOWN_SERVER_ERROR"]
+			], 500);
+		} else {
+			DB::commit();
+		}
+
+		return \Response::json([
+			"success" => true
+		], 201);
+	}
+
+	private function generateValidationRules(bool $validateResidence = false, bool $validateChildrenRegistryId = false, ?Student $student = null)
+	{
+		$validationRules = ["firstName" => ["required", "max:255"],
+			"lastName" => ["required", "max:255"],
+			"secondName" => ["nullable", "max:255"],
+			"pesel" => [
+				"required_without:alternateIdentityDocument",
+				$student != null ? Rule::unique("students")->ignore($student->id) : "unique:students",
+				new Pesel
+			],
+			"alternateIdentityDocument" => [
+				"required_without:pesel",
+				"max:255",
+				$student != null ?
+					Rule::unique("students", "alternate_identity_document")->ignore($student->id) :
+					"unique:students,alternate_identity_document"
+			],
+			"birthdate" => ["required", "date"],
+			"birthplace" => ["required", "max:255"],
+			"gender" => ["required", "in:male,female"],
+			"admissionDate" => ["required", "date"],
+		];
+
+		if ($validateResidence) {
+			$validationRules["residenceAddressCountry"] = ["required", "max:255"];
+			$validationRules = array_merge($validationRules, array_fill_keys([
+				"residenceAddressCommune",
+				"residenceAddressTown",
+				"residenceAddressPostalCode",
+				"residenceAddressHouseNumber",
+				"residenceAddressStreet"
+			], ["nullable", "max:255"]));
+		}
+
+		if ($validateChildrenRegistryId) {
+			$validationRules["childrenRegistryId"] = ["nullable", "exists:children_registries,id"];
+		}
+
+		return $validationRules;
+	}
+
+	private function createAndSaveStudentWithResidenceAddress(array $data, ?bool &$transactionStatus = null)
+	{
+		$residenceAddress = new ResidenceAddress();
+		$residenceAddress->country = $data["residenceAddressCountry"];
+		$residenceAddress->commune = $data["residenceAddressCommune"];
+		$residenceAddress->town = $data["residenceAddressTown"];
+		$residenceAddress->postal_code = $data["residenceAddressPostalCode"];
+		$residenceAddress->house_number = $data["residenceAddressHouseNumber"];
+		$residenceAddress->street = $data["residenceAddressStreet"];
+		$residenceAddress->save();
+
+		$student = new Student();
+		$student->first_name = $data["firstName"];
+		$student->last_name = $data["lastName"];
+		$student->second_name = $data["secondName"] ?? null;
+		$student->pesel = $data["pesel"] ?? null;
+		$student->alternate_identity_document = $data["alternateIdentityDocument"] ?? null;
+		$student->birthdate = $data["birthdate"];
+		$student->birthplace = $data["birthplace"];
+		$student->gender = $data["gender"];
+		$student->admission_date = $data["admissionDate"];
+		$student->residence_address_id = $residenceAddress->id;
+
+		try {
+			$student->saveOrFail();
+		} catch (\Throwable) {
+			// Saving the student failed, so we need to delete the residence address
+			if ($transactionStatus != null) {
+				$transactionStatus = false;
+			}
+			$residenceAddress->delete();
+		}
+
+		return $student;
 	}
 }
