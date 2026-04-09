9798833bcfdf059d11e10c44ccbc5fca9897a905
Piotr Badełek <piotrbadelek@protonmail.com>
2026-04-09 22:01:01 +0200

    Add system verioning for KU/KED tables

--- database/migrations/2026_02_02_094126_create_children_registries_table.php
+++ database/migrations/2026_02_02_094126_create_children_registries_table.php
@@ -27,5 +27,6 @@ return new class extends Migration {
 	public function down(): void
 	{
 		Schema::dropIfExists("children_registries");
+		Schema::dropIfExists("children_registry_student");
 	}
 };
