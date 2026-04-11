b6800cb6cf814826e7df9ae7b958356628f7819b
Piotr Badełek <piotrbadelek@protonmail.com>
2026-04-11 19:42:51 +0200

    Refactor tests to remove repetitive header declarations and user generation

--- tests/Feature/Http/Controllers/StudentRegistryControllerTest.php
+++ tests/Feature/Http/Controllers/StudentRegistryControllerTest.php
@@ -15,30 +15,15 @@ class StudentRegistryControllerTest extends TestCase
 {
 	use RefreshDatabase;
 
-	private function actingUser(): array
-	{
-		$user = User::factory()->create();
-		$this->be($user);
-		$employee = Employee::factory()->create([
-			"is_admin" => true,
-			"is_headmaster" => true
-		]);
-		$accountAccess = AccountAccess::create();
-		$accountAccess->employee_id = $employee->id;
-		$accountAccess->user_id = $user->id;
-		$accountAccess->save();
-		return ["user" => $user, "access" => $accountAccess->id];
-	}
-
 	public function test_can_list_student_registries()
 	{
-		$actingUser = $this->actingUser();
+		$this->actingUser();
 		$schoolComplex = SchoolComplex::factory()->create();
 		$schoolUnit = SchoolUnit::factory()->create([
 			"school_complex_id" => $schoolComplex->id
 		]);
 		$registry = $schoolUnit->studentRegistry()->create();
-		$response = $this->get("/api/studentRegistry", ["Access-ID" => $actingUser["access"]]);
+		$response = $this->get("/api/studentRegistry");
 		$response->assertOk();
 		$response->assertJsonIsArray();
 		$response->assertJsonFragment([
@@ -49,14 +34,14 @@ class StudentRegistryControllerTest extends TestCase
 
 	public function test_can_create_a_student_registry()
 	{
-		$actingUser = $this->actingUser();
+		$this->actingUser();
 		$schoolComplex = SchoolComplex::factory()->create();
 		$schoolUnit = SchoolUnit::factory()->create([
 			"school_complex_id" => $schoolComplex->id
 		]);
 		$response = $this->post("/api/studentRegistry", [
 			"schoolUnitId" => $schoolUnit->id
-		], ["Access-ID" => $actingUser["access"]]);
+		]);
 		$response->assertCreated();
 		$this->assertDatabaseHas("student_registries", [
 			"school_unit_id" => $schoolUnit->id,
@@ -65,10 +50,10 @@ class StudentRegistryControllerTest extends TestCase
 
 	public function test_cannot_create_a_student_registry_without_a_valid_school_unit_id()
 	{
-		$actingUser = $this->actingUser();
+		$this->actingUser();
 		$response = $this->post("/api/studentRegistry", [
 			"schoolUnitId" => "invalid-id"
-		], ["Access-ID" => $actingUser["access"]]);
+		]);
 		$response->assertUnprocessable();
 		$this->assertDatabaseMissing("student_registries", [
 			"school_unit_id" => "invalid-id"
@@ -76,7 +61,7 @@ class StudentRegistryControllerTest extends TestCase
 	}
 
 	public function test_cannot_create_a_student_registry_when_the_school_unit_already_has_one() {
-		$actingUser = $this->actingUser();
+		$this->actingUser();
 		$schoolComplex = SchoolComplex::factory()->create();
 		$schoolUnit = SchoolUnit::factory()->create([
 			"school_complex_id" => $schoolComplex->id
@@ -84,7 +69,7 @@ class StudentRegistryControllerTest extends TestCase
 		$schoolUnit->studentRegistry()->create();
 		$response = $this->post("/api/studentRegistry", [
 			"schoolUnitId" => $schoolUnit->id
-		], ["Access-ID" => $actingUser["access"]]);
+		]);
 		$response->assertStatus(409);
 	}
 }
