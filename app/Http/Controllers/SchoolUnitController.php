c5b8bf1b657c932508417d8e163e2499dcdd7d3b
Marioneq 4958 <mq4958@gmail.com>
2026-06-29 12:17:29 +0200

    Clean up SchoolUnitController

--- app/Http/Controllers/SchoolUnitController.php
+++ app/Http/Controllers/SchoolUnitController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\SchoolType;
-use App\Enums\Voivodeship;
+use App\Http\Requests\SchoolUnitRequest;
 use App\Models\SchoolUnit;
-use App\Utilities\ValidatorAssistant\ValidatorAssistant;
+use App\Utilities\CaseConverter;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Validation\Rule;
 
 class SchoolUnitController extends Controller
 {
@@ -17,17 +15,9 @@ class SchoolUnitController extends Controller
 		return SchoolUnit::all()->toResourceCollection();
 	}
 
-	public function create(Request $request) {
-		$data = $this->validateSchoolUnit($request);
-
-		if ($data["studentCategory"] != "childrenAndYouths" && $data["studentCategory"] != "adultsOnly") {
-			return Response::json([
-				"success" => false,
-				"errors" => [
-					"INVALID_STUDENT_CATEGORY"
-				]
-			], 400);
-		}
+	public function create(SchoolUnitRequest $request)
+	{
+		$data = $request->validated();
 
 		if (SchoolUnit::count() > 1 && $data["schoolComplexId"] == null) {
 			return Response::json([
@@ -38,27 +28,13 @@ class SchoolUnitController extends Controller
 			]);
 		}
 
-		$unit = new SchoolUnit();
-		$unit->name = $data["name"];
-		$unit->type = $data["type"];
-		$unit->student_category = $data["studentCategory"];
-		$unit->municipality = $data["municipality"];
-		$unit->voivodeship = $data["voivodeship"];
-		$unit->town = $data["town"];
-		$unit->district = $data["district"];
-		$unit->postal_code = $data["postalCode"];
-		$unit->street = $data["street"];
-		$unit->house_number = $data["houseNumber"];
-		$unit->flat_number = $data["flatNumber"];
-		$unit->short_name = $data["shortName"];
-		$unit->school_complex_id = $data["schoolComplexId"];
-		$unit->save();
-		return \Response::json([
-			"success" => true
-		], 201);
+		SchoolUnit::create(CaseConverter::toSnakeCase($data));
+
+		Response::json(["success" => true], 201);
 	}
 
-	public function update(Request $request, SchoolUnit $schoolUnit) {
+	public function update(SchoolUnitRequest $request, SchoolUnit $schoolUnit)
+	{
 		if (!$schoolUnit->active) {
 			return Response::json([
 				"success" => false,
@@ -68,65 +44,22 @@ class SchoolUnitController extends Controller
 			]);
 		}
 
-		$data = $this->validateSchoolUnit($request);
+		$data = $request->validated();
+		$schoolUnit->create(CaseConverter::toSnakeCase($data));
 
-		if ($data["studentCategory"] != "childrenAndYouths" && $data["studentCategory"] != "adultsOnly") {
-			return Response::json([
-				"success" => false,
-				"errors" => [
-					"INVALID_STUDENT_CATEGORY"
-				]
-			], 400);
-		}
-
-		$schoolUnit->name = $data["name"];
-		$schoolUnit->type = $data["type"];
-		$schoolUnit->student_category = $data["studentCategory"];
-		$schoolUnit->municipality = $data["municipality"];
-		$schoolUnit->voivodeship = $data["voivodeship"];
-		$schoolUnit->town = $data["town"];
-		$schoolUnit->district = $data["district"];
-		$schoolUnit->postal_code = $data["postalCode"];
-		$schoolUnit->street = $data["street"];
-		$schoolUnit->house_number = $data["houseNumber"];
-		$schoolUnit->flat_number = $data["flatNumber"];
-		$schoolUnit->short_name = $data["shortName"];
-		$schoolUnit->school_complex_id = $data["schoolComplexId"];
-		$schoolUnit->save();
-
-		return [
-			"success" => true
-		];
+		Response::json(["success" => true]);
 	}
 
-	public function archive(Request $request, SchoolUnit $schoolUnit) {
-		$validator = ValidatorAssistant::validate($request, [
+	public function archive(Request $request, SchoolUnit $schoolUnit)
+	{
+		$data = $request->validate([
 			"password" => "required|current_password",
 			"state" => "required|boolean"
 		]);
 
-		$schoolUnit->active = $validator["state"];
+		$schoolUnit->active = $data["state"];
 		$schoolUnit->save();
-		return [
-			"success" => true
-		];
-	}
 
-	private function validateSchoolUnit(Request $request) {
-		return ValidatorAssistant::validate($request, [
-			"name" => ["required", "max:255"],
-			"type" => ["required", Rule::enum(SchoolType::class)],
-			"studentCategory" => ["required"],
-			"municipality" => ["required", "max:255"],
-			"voivodeship" => ["required", Rule::enum(Voivodeship::class)],
-			"town" => ["nullable", "max:255"],
-			"district" => ["nullable", "max:255"],
-			"postalCode" => ["required", "max:7"],
-			"street" => ["required", "max:255"],
-			"houseNumber" => ["required", "max:255"],
-			"flatNumber" => ["nullable", "max:255"],
-			"shortName" => ["required", "max:255"],
-			"schoolComplexId" => ["nullable", "exists:school_complexes,id"]
-		]);
+		Response::json(["success" => true]);
 	}
 }
---
+++ app/Http/Requests/SchoolUnitRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\SchoolType;
+use App\Enums\Voivodeship;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class SchoolUnitRequest extends FormRequest
+{
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, ValidationRule|array<mixed>|string>
+	 */
+	public function rules(): array
+	{
+		return [
+			"name" => ["required", "max:255"],
+			"type" => ["required", Rule::enum(SchoolType::class)],
+			"studentCategory" => ["required", Rule::in(['childrenAndYouths', 'adultsOnly'])],
+			"municipality" => ["required", "max:255"],
+			"voivodeship" => ["required", Rule::enum(Voivodeship::class)],
+			"town" => ["nullable", "max:255"],
+			"district" => ["nullable", "max:255"],
+			"postalCode" => ["required", "max:7"],
+			"street" => ["required", "max:255"],
+			"houseNumber" => ["required", "max:255"],
+			"flatNumber" => ["nullable", "max:255"],
+			"shortName" => ["required", "max:255"],
+			"schoolComplexId" => ["nullable", "exists:school_complexes,id"]
+		];
+	}
+}
